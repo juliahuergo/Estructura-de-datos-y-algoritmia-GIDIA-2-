142babd79726667e42834fdd2a3d00f06fbf73e8
Julia Huergo Muñoz <95702056+juliahuergo@users.noreply.github.com>
2025-11-03 16:56:05 +0100

    Add files via upload

---
+++ 5.3-REORDENA.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+bool casoDePrueba() {
+	int N;
+	cin >> N;
+
+	if (N == 0)
+		return false;
+
+	//guardamos en pila de forma creciente
+	//si elem < cima --> va a cola
+
+	return true;
+}
+
+int main() {
+	while (casoDePrueba());
+	return 0;
+}
---
+++ 5.5-MENSAJES.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <deque>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool casoDePrueba() {
+	string cifrado;
+	getline(cin,cifrado);
+
+	if (!cin)
+		return false;
+
+	deque<char> pila1, pila2;
+
+	for (int i = 0; i < cifrado.size(); i++) {
+		if (i % 2 == 0)
+			pila1.push_back(cifrado[i]);
+		else
+			pila2.push_back(cifrado[i]);
+	}
+
+	while (!pila2.empty()) {
+		pila1.push_back(pila2.back());
+		pila2.pop_back();
+	}
+
+	//Ahora está X' en pila1
+	stack<char> aux;
+	while (!pila1.empty()) {
+		if (tolower(pila1.front()) == 'a' || tolower(pila1.front()) == 'e' || tolower(pila1.front()) == 'o' || tolower(pila1.front()) == 'i' || tolower(pila1.front()) == 'u') {
+			cout << pila1.front();
+			pila1.pop_front();
+		}
+		else {
+			
+			while (!(tolower(pila1.front()) == 'a' || tolower(pila1.front()) == 'e' || tolower(pila1.front()) == 'o' || tolower(pila1.front()) == 'i' || tolower(pila1.front()) == 'u')) {
+				aux.push(pila1.front()); 
+				pila1.pop_front();
+			}
+			while (!aux.empty()) {
+				cout << aux.top(); 
+				aux.pop();
+			}
+		}
+	}
+
+	while (!aux.empty()) {
+		cout << aux.top();
+		aux.pop();
+	}
+
+	cout << "\n";
+
+	return true;
+}
+
+int main() {
+	while (casoDePrueba());
+	return 0;
+}
